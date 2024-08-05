3be7c6732c9480a9810a01579580deca7abc9506
msbCyricTohoku <ben.sh@tohoku.ac.jp>
2024-08-05 18:43:08 +0800

    cleaned directories

--- working_dir/run_pos.sh
+++ working_dir/run_pos.sh
@@ -16,3 +16,21 @@ phits.sh  position_2/position_2.inp
 mv *.out *.eps ./position_2
 phits.sh  position_3/position_3.inp
 mv *.out *.eps ./position_3
+phits.sh  position_1/position_1.inp
+mv *.out *.eps ./position_1
+phits.sh  position_2/position_2.inp
+mv *.out *.eps ./position_2
+phits.sh  position_3/position_3.inp
+mv *.out *.eps ./position_3
+phits.sh  position_1/position_1.inp
+mv *.out *.eps ./position_1
+phits.sh  position_2/position_2.inp
+mv *.out *.eps ./position_2
+phits.sh  position_3/position_3.inp
+mv *.out *.eps ./position_3
+phits.sh  position_1/position_1.inp
+mv *.out *.eps ./position_1
+phits.sh  position_2/position_2.inp
+mv *.out *.eps ./position_2
+phits.sh  position_3/position_3.inp
+mv *.out *.eps ./position_3
